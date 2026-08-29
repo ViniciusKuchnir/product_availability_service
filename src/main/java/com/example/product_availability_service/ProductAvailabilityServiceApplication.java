e49c69df98f0ebfddd2671dd23652feb273225e9
Vinicius Kuchnir <vina200902@gmail.com>
2026-08-29 13:28:30 -0300

    init: initial commit

---
+++ src/main/java/com/example/product_availability_service/ProductAvailabilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.product_availability_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAvailabilityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductAvailabilityServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/product_availability_service/ProductAvailabilityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.product_availability_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductAvailabilityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
